1ed3809caf2587b941ecafa1d8fdaf100b976f7f
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-05 13:25:30 +0500

    Module

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-// global objects
 /*
+// global objects
 console.log(); 
 setTimeout();
 clearInterval();
 setInterval();
 clearInterval();
-*/
 
 let message = '';
 console.log(global.message); //undefined
+*/
+
+let sayHello = function () {
+
+}
+
+// window.sayHello();
+console.log(module);
