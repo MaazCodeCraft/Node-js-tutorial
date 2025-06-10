b0cd5b79b3478793b104cec0dec0dd4e6e15dc51
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-10 14:48:16 +0500

    HTTP Module

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const EventEmitter = require('events');
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
 
-const Logger = require('./logger');
-const logger = new Logger();
-
-// Register a listener
-logger.on ('messageLogged', (arg) => {
-    console.log('Listener Called!', arg);
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1, 2, 3, 4]));
+        res.end();
+    }
 });
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
